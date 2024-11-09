a9de805f63a9446d16c2cd87c1023b931c0195bf
Hrithic <hrithicraj730@gmail.com>
2024-11-09 17:36:41 +0530

    address error handling updated

--- src/services/addressService.js
+++ src/services/addressService.js
@@ -17,13 +17,14 @@ exports.addAddress = async (userId, newAddress)=>{
 
 exports.getAddressById = async (userId) =>{
     const userAddress = await Address.find({userId});
-    if(!userAddress) throw new Error('No address found');
+    if(!userAddress) throw new Error('address not found');
     return userAddress;
 }
 
 exports.updateAddress = async (userId, addressId, addressData) =>{
     const address = await Address.findOne({userId})
     const addressIndex = address.allAddress.findIndex(item => item._id.equals(addressId))
+    if(addressIndex === -1) throw new Error('address not found');
     address.allAddress[addressIndex] = addressData;
     
     return await address.save();
@@ -32,6 +33,7 @@ exports.updateAddress = async (userId, addressId, addressData) =>{
 exports.deleteAddress = async (userId, addressId) =>{
     let address = await Address.findOne({userId})
     const addressIndex = address.allAddress.findIndex(item => item._id.equals(addressId))
-    address.allAddress.splice(addressIndex,1);
+    if(addressIndex === -1) throw new Error('address not found');
+    address.allAddress.splice(addressIndex, 1);
     return await address.save();
 }
